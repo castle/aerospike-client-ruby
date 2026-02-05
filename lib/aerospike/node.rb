fddc159898b4a70db3345efd86dca1db6e1a8e55
Sebastian Zuchmanski <sebastian.zuchmanski@castle.io>
2026-02-05 15:17:34 +0100

    Remove timeout setting for connection in tend_connection method

--- lib/aerospike/node.rb
+++ lib/aerospike/node.rb
@@ -120,8 +120,6 @@ module Aerospike
     def tend_connection
       if @tend_connection.nil? || @tend_connection.closed?
         @tend_connection = Cluster::CreateConnection.(cluster, host)
-        @tend_connection.timeout = cluster.connection_timeout
-        @tend_connection
       end
       @tend_connection
     end
